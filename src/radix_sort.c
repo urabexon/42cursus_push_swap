58d7821cf50992ca76622f0295042b96e8d5cc47
Hiroki Urabe <hurabe@c3r4s3.42tokyo.jp>
2024-09-18 22:51:17 +0900

    add

--- src/radix_sort.c
+++ src/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 20:41:37 by hurabe            #+#    #+#             */
-/*   Updated: 2024/09/17 21:08:17 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/09/18 21:41:42 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,29 @@ static long	get_top(t_dst *dst)
 
 void	radix_sort(t_dst **a, t_dst **b, t_config *conf)
 {
-	
+	int		max_bits;
+	int		i;
+	int		j;
+	long	num;
+
+	max_bits = 0;
+	while ((conf->max_num >> max_bits) != 0)
+		max_bits++;
+	i = 0;
+	while (i < max_bits && !is_sorted_ascending(a))
+	{
+		j = 0;
+		while (j < conf->len)
+		{
+			num = get_top(*a);
+			if (num & (1 << i))
+				ra(a);
+			else
+				pb(a, b);
+			j++;
+		}
+		while (*b)
+			pa(a, b);
+		i++;
+	}
 }
